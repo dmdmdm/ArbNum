525ce2c69e02a0b1697828cb87f4de6d7c9a7cc4
dmdmdm <3187057+dmdmdm@users.noreply.github.com>
2023-03-03 12:56:40 -0500

    Update bc.cpp

--- bc.cpp
+++ bc.cpp
@@ -30,8 +30,6 @@ int main(const int argc, char* argv[]) {
     if (strcmp(argv[1], "-?") == 0) usage();
 
     if (strcmp(argv[1], "-t") == 0) {
-      printf("Running tests\n");
-      exit(0);  // TEST
       const time_t start = time(NULL);
       const bool result = ArbNum::testAll();
       const time_t duration = time(NULL) - start;
